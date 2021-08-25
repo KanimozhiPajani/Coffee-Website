ad699a5e343a652a734da503f14a38c9dd369f29
Kanimozhi <kanimozhi.kp.214@gmail.com>
2021-08-25 23:03:16 +0530

    Complete website

--- Coffee website/Js/script.js
+++ Coffee website/Js/script.js
@@ -1,7 +1,15 @@
+let navBar = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick = () => {
+  navBar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
 let searchForm = document.querySelector(".search-form");
 
 document.querySelector("#search-btn").onclick = () => {
   searchForm.classList.toggle("active");
+  navBar.classList.remove("active");
   cartItem.classList.remove("active");
 };
 
@@ -9,5 +17,12 @@ let cartItem = document.querySelector(".cart-item-container");
 
 document.querySelector("#cart-btn").onclick = () => {
   cartItem.classList.toggle("active");
+  navBar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navBar.classList.remove("active");
+  cartItem.classList.remove("active");
   searchForm.classList.remove("active");
 };
